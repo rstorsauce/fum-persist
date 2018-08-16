89b657e494b0ab068f128c0e550a2b54d7628180
Isaac <isaac.yonemoto@gmail.com>
2018-08-15 20:54:41 -0700

    finished full lifecycle worth of unit tests against the fum

--- fum.py
+++ fum.py
@@ -14,7 +14,6 @@ resp_port = int(os.getenv('FUM_PORT', 0))
 log_file = sys.stderr #open("/tmp/fumlog.txt", "a")
 
 def eprint(*args):
-    global log_file
     print(*args, file=log_file)
     log_file.flush()
 
@@ -30,81 +29,51 @@ def is_uuid(uuid):
                 return False
     return True
 
-#set up.
-#if port:
-#  eprint("setting up communication socket")
-#  try:
-#    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#  except:
-#    eprint("fum_yield failed to create a socket")
-#    exit(1)
-#  server_address = (node_ip, port)
-#  sock.bind(server_address)
-#  sock.listen(1)
-
-def internal_fum_yield(has_run, port, sock, node, resp_host, resp_port):
-    if port:
-        while True:
-            eprint('signalling ok to', resp_host, 'at', resp_port)
-            try:
-                #transiently create an output socket.
-                resp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                server_address = (resp_host, resp_port)
-                resp_sock.connect(server_address)
-                resp_sock.sendall('ok'.encode())
-                break
-            except:
-                #keep pinging the socket until we get a response.
-                eprint('failed to send ok signal, going to retry.')
-                time.sleep(1)
-            finally:
-                resp_sock.close()
+def fum_node_yields__(resp_host, resp_port):
+    while True:
+        eprint('signalling ok to', resp_host, 'at', resp_port)
+        try:
+            #transiently create an output socket.
+            resp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            server_address = (resp_host, resp_port)
+            resp_sock.connect(server_address)
+            resp_sock.sendall('ok'.encode())
+            break
+        except:
+            #keep pinging the socket until we get a response.
+            eprint('failed to send ok signal, going to retry.')
+            time.sleep(1)
+        finally:
+            resp_sock.close()
     return 0
 
-#def fum_yield():
-#    if port:
-#        while True:
-#            eprint("signalling ok to", resp_host, "at", resp_port)
-#      try:
-#        #transiently create an output socket.
-#        resp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#        server_address = (resp_host, resp_port)
-#        resp_sock.connect(server_address)
-#        resp_sock.sendall(bytes(node,"ascii"))
-#        eprint("sent ok signal to host")
-#        break
-#      except:
-#        #keep pinging the socket until we successfully get a response.
-#        eprint("failed to send ok signal to host")
-#        time.sleep(1)
-#      finally:
-#        resp_sock.close()
-#
-#    eprint("waiting for job signal...")
-#    connection, client_address = sock.accept()
-#    try:
-#      #a uuid should be 36 bytes of data.
-#      data = connection.recv(36).decode("utf-8")
-#      if is_uuid(data):
-#        connection.sendall(bytes("ok", "ascii"))
-#        eprint("triggering job", data)
-#        os.listdir("/input")
-#      elif data == "no":
-#        eprint("terminate signal received")
-#        connection.close()
-#        sock.close()
-#        exit(0)
-#      else:
-#        eprint("strange data recieved")
-#        connection.close()
-#        sock.close()
-#        exit(1)
-#    finally:
-#      connection.close()
-#  else:
-#    eprint("persistent mode not detected, running singly")
-#    if has_run:
-#      exit(0)
-#    else:
-#      has_run = True
-#
+def fum_node_waits__(sock):
+    eprint("waiting for job signal...")
+    connection, client_address = sock.accept()
+    try:
+        #a uuid should be 36 bytes of data.
+        data = connection.recv(36).decode("utf-8")
+        if is_uuid(data):
+            connection.sendall('ok'.encode())
+            eprint("triggering job", data)
+            return data
+        elif data == "done":
+            eprint("terminate signal received")
+            connection.close()
+            sock.close()
+            exit(0)
+        else:
+            eprint("strange data recieved")
+            connection.close()
+            sock.close()
+            exit(1)
+    finally:
+        connection.close()
+
+def fum_yield__(has_setup, node_port, socket, node_name, resp_host, resp_port):
+    if resp_port:
+        fum_node_yields__(resp_host, resp_port)
+        fum_node_waits__(socket)
+    else:
+        eprint("persistent mode not detected, running singly")
+    return 0
--- tests.py
+++ tests.py
@@ -3,14 +3,13 @@ import sys
 import unittest
 import time
 import uuid
-from fum import is_uuid
-from fum import internal_fum_yield
+from fum import *
 from random import randint
 import concurrent.futures
 
 import asyncio
 
-def build_yield_socket(port):
+def build_socket(port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except:
@@ -21,13 +20,18 @@ def build_yield_socket(port):
     sock.listen(1)
     return sock
 
-def socket_listener(host_port, numbytes):
-    # Create a TCP/IP socket
+def send_uuid(port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # Bind the socket to the port
-    server_address = ('localhost', host_port)
-    sock.bind(server_address)
-    sock.listen(1)
+    server_address = ('localhost', port)
+    sock.connect(server_address)
+    rand_uuid = str(uuid.uuid4())
+    sock.sendall(rand_uuid.encode())
+    data = sock.recv(2)
+    sock.close()
+    return data, rand_uuid
+
+def socket_listener(host_port, numbytes):
+    sock = build_socket(host_port)
     connection, client_address = sock.accept()
     data = connection.recv(numbytes)
     connection.close()
@@ -50,20 +54,29 @@ class TestUUID(unittest.TestCase):
 
 class TestFumYieldSuccess(unittest.TestCase):
     def test_no_blocking_when_no_port(self):
-        res = internal_fum_yield(False, 0, None, '0', '', 0)
+        res = fum_yield__(False, 0, None, '0', '', 0)
         self.assertTrue(0 == res)
 
-    def test_node_responds_with_ok_when_ready(self):
-        port = randint(10000,11000)
-        sock = build_yield_socket(port)
-        resp_port = randint(11000, 12000)
+    def test_yield_responds_with_ok_when_ready(self):
+        resp_port = randint(11000, 11999)
         with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
             server_future = executor.submit(socket_listener, resp_port, 2)
             time.sleep(0.01)
-            fyr = internal_fum_yield(False, port, sock, 0, 'localhost', resp_port)
+            fyr = fum_node_yields__('localhost', resp_port)
             self.assertTrue(0 == fyr)
             self.assertTrue(b'ok' == server_future.result())
-            sock.close()
+
+    def test_wait_responds_with_ok_when_delivered(self):
+        node_port = randint(12000, 12999)
+        sock = build_socket(node_port)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+            node_future = executor.submit(fum_node_waits__, sock)
+            time.sleep(0.01)
+            host_res, rand_uuid = send_uuid(node_port)
+            self.assertTrue(b'ok' == host_res)
+            self.assertTrue(rand_uuid == node_future.result())
+        sock.close()
+
 
 if __name__ == '__main__':
     unittest.main()
