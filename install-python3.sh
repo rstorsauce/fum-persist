a0f54118213dce62c2e60bf48c09bef76489461f
Isaac <isaac.yonemoto@gmail.com>
2018-10-29 12:15:21 -0700

    wrong oop

--- install-python3.sh
+++ install-python3.sh
@@ -2,8 +2,9 @@
 
 cd python3
 
-python3 setup.py sdist
-
 # TODO: get this out of the shell script and into a proper pip dependency
 pip install uuid
+
+python3 setup.py sdist
+
 pip install dist/fum-persist-0.0.1.tar.gz
